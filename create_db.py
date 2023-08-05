59e026e62f7ea748a681e08889bd0c802480d936
Andrej Roštek <andrej@wattstor.com>
2023-08-05 15:35:57 +0200

    db in parts

--- create_db.py
+++ create_db.py
@@ -3,9 +3,9 @@ import json
 from sherdog_parser import scrape_all_fighters
 
 
-db_name = "sherdog_db"
+db_name = "sherdog_db_part2"
 
-with open(f"{db_name}.json", 'r') as file:
+with open(f"sherdog_db.json", 'r') as file:
     data = json.load(file)
 
 if data == {}:
@@ -14,4 +14,4 @@ else:
     last_entry = next(reversed(data.keys()))
     last_figter_id = data[last_entry]["id"]
 
-scrape_all_fighters(filename=db_name, filetype="json", fighter_index=last_figter_id)
+scrape_all_fighters(filename=db_name, filetype="json", fighter_index=last_figter_id+1)
